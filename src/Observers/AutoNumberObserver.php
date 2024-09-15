40a7674d0635d2af2383021140126442e7190b10
Shihan <shmshihan@yahoo.com>
2024-09-15 20:10:52 +0800

    Create AutoNumberObserver.php

---
+++ src/Observers/AutoNumberObserver.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Shan016\AutoNumber\Observers;
+
+use Shan016\AutoNumber\AutoNumber;
+use Illuminate\Database\Eloquent\Model;
+
+class AutoNumberObserver
+{
+    /**
+     * @var AutoNumber
+     */
+    private $autoNumber;
+
+    /**
+     * AutoNumberObserver constructor.
+     *
+     * @param AutoNumber $autoNumber
+     */
+    public function __construct(AutoNumber $autoNumber)
+    {
+        $this->autoNumber = $autoNumber;
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return null
+     */
+    public function saving(Model $model)
+    {
+        if (! config('autonumber.onUpdate', false) && $model->exists) {
+            return;
+        }
+
+        $this->generateAutoNumber($model);
+    }
+
+    /**
+     * Generate auto number.
+     *
+     * @param Model $model
+     * @return bool
+     */
+    protected function generateAutoNumber(Model $model)
+    {
+        $generated = $this->autoNumber->generate($model);
+
+        return $generated;
+    }
+}
